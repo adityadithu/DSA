a83fb728c3811a55bcac79311090706f06d32854
adityadithu <144141857+adityadithu@users.noreply.github.com>
2026-03-06 01:06:44 +0530

    Add files via upload

--- Heap/buildheap.cpp
+++ Heap/buildheap.cpp
@@ -1,1 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+void heapfiy(vector<int>&heap,int i)
+{   int n = heap.size();
+    while(true)
+    {
+        int left = 2*i+1;
+        int right = 2*i+2;
+        int smallest = i;
+        if(left <n && heap[left]<heap[smallest])
+        {
+            smallest= left;
+        }
+        if(right<n && heap[right]<heap[smallest])
+        {
+            smallest = right;
+        }
+        if(smallest!=i)
+        {
+            swap(heap[i],heap[smallest]);
+            i = smallest;
+        }
+        else{
+            break;
+        }
+    }
+}
+void build(vector<int>&heap)
+{
+    int n = heap.size();
+    for(int i = n/2-1;i>=0;i--)
+    {
+        heapfiy(heap,i);
+    }
 
+}
+int main()
+{
+    vector<int> heap = {12,7,25,15,28,33};
+    build(heap);
+    for(int x:heap)
+    {
+        cout<<x<<" ";
+    }
+}
